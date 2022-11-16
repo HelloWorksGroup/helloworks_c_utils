1f0b0bcdc647139731374b21e31b48d60eb6f990
xianii <xianyi.xia@outlook.com>
2022-11-16 11:36:58 +0800

    update format.ps

--- format.ps1
+++ format.ps1
@@ -1,1 +1,7 @@
-clang-format -i *.c *.h -style=file --verbose
+#Requires -Version 3.0
+$srcfiles = @()
+Get-ChildItem -File -Recurse | where {$_.extension -in ".c",".h"} | 
+	ForEach {
+		$srcfiles += $_.FullName
+	}
+powershell "clang-format -style=file --verbose -i ${srcfiles}"
